e29f450aadc0459fd3dc0a68c0da9969d4fea37f
Linda Chang <lindachang186@gmail.com>
2019-02-03 18:37:54 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path =="/items/"
+      @@items.find do |item|
+        resp.write item.price
+      end
+    else
+        resp.write "Route not found"
+        resp.status = 404
+    end
+
+
+
+    resp.finish
+  end
+
+
+end
